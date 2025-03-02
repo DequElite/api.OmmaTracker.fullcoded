e15cbadc2d792d1eb83ba5af10b5dedd7817ee12
2024MAX <bigukrcode@gmail.com>
2025-03-02 18:03:18 +0200

    Error fixing 400 GoogleAuth | attempt 3

--- dist/routes/register/Google/GoogleAuth.js
+++ dist/routes/register/Google/GoogleAuth.js
@@ -23,14 +23,24 @@ require("dotenv").config();
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "";
+const APP_MODE = process.env.APP_MODE || "DEV";
+const CALLBACK_URL = APP_MODE === "PROD"
+    ? process.env.CALLBACK_URL_PROD
+    : process.env.CALLBACK_URL_DEV;
+const CLIENT_URL = APP_MODE === "PROD"
+    ? process.env.CLIENT_URL_PROD
+    : process.env.CLIENT_URL_DEV;
 passport_1.default.use(new passport_google_oauth20_1.Strategy({
     clientID: GOOGLE_CLIENT_ID || "",
     clientSecret: GOOGLE_CLIENT_SECRET || "",
-    callbackURL: '/api/register/google/callback',
+    callbackURL: CALLBACK_URL,
     passReqToCallback: true
 }, (_req, accessToken, refreshToken, profile, done) => __awaiter(void 0, void 0, void 0, function* () {
     const email = profile.emails ? profile.emails[0].value : "";
     const username = profile.username || profile.displayName || email.split('@')[0];
+    console.log("APP_MODE:", APP_MODE);
+    console.log("CALLBACK_URL:", CALLBACK_URL);
+    console.log("CLIENT_URL:", CLIENT_URL);
     try {
         let user = yield db_1.pool.query(`
                     SELECT * FROM Users 
@@ -76,13 +86,13 @@ GoogleAuth.get('/google/callback', passport_1.default.authenticate("google", { s
         res.status(401).json({ error: "Authorization failed" });
         return;
     }
-    const { refreshToken, accesToken } = req.user;
+    const { refreshToken, accessToken } = req.user;
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: process.env.APP_MODE === "PROD",
         maxAge: 7 * 24 * 60 * 60 * 1000,
         sameSite: process.env.APP_MODE === "DEV" ? "strict" : "none",
     });
-    res.redirect(`http://localhost:5173/home?accessToken=${accesToken}`);
+    res.redirect(`${CLIENT_URL}/home?accessToken=${accessToken}`);
 });
 exports.default = GoogleAuth;
