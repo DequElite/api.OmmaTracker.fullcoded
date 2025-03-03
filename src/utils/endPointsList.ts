385db318d3a3d2709f58d355beb7350d6d2e375d
2024MAX <bigukrcode@gmail.com>
2025-03-03 09:25:17 +0200

    CI/CD pipline | attempt 1

--- src/utils/endPointsList.ts
+++ src/utils/endPointsList.ts
@@ -5,7 +5,7 @@ const listEndpoints = require("express-list-endpoints");
 
 export default function ReturnEndPoints(app: Express): void {
     const endpoints = listEndpoints(app);
-    console.log(colorize('Available routes:', 'yellow', 'black', 'italic'));
+    console.log(colorize('Available routes:', 'blue', 'black', 'italic'));
     endpoints.forEach((endpoint:any) => {
         console.log(colorize(`${endpoint.methods.join(', ')} ${endpoint.path}`, 'yellow', 'black', 'italic'));
     });
